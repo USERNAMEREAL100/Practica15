a6112121eb6203efc3183d19defd7a2f099b7d51
USERNAMEREAL100 <127427691+USERNAMEREAL100@users.noreply.github.com>
2025-06-17 10:50:07 +0200

    Add files via upload

---
+++ GameSettings.java
@@ -0,0 +1,11 @@
+class GameSettings {
+    String playerXName;
+    String playerOName;
+    int boardSize;
+
+    public GameSettings(String playerXName, String playerOName, int boardSize) {
+        this.playerXName = playerXName;
+        this.playerOName = playerOName;
+        this.boardSize = boardSize;
+    }
+}
